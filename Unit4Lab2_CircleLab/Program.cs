08665e1ac956d4629f4a2ea8b5d93c7a2b6b4670
amandabledsoe <amandajbledsoe@gmail.com>
2022-05-12 18:48:54 -0400

    initial commit

---
+++ Unit4Lab2_CircleLab/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Circle 
+{
+    public double Radius;
+    public double Circumference;
+    public double Area;
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public double CalculateCircumference()
+    {
+        return Circumference;
+    }
+
+    public string CalculateFormattedCircumference()
+    {
+        return "";
+    }
+
+    public double CalculateArea()
+    {
+        return Area;
+    }
+
+    public string CalculateFormattedArea()
+    {
+        return "";
+    }
+
+    private string FormatNumber(double x)
+    {
+        return x.ToString();
+    }
+}
