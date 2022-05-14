49d52e63746d557bc2a617d69dc7098fa554f446
amandabledsoe <amandajbledsoe@gmail.com>
2022-05-14 10:36:09 -0400

    additional updates to circle class and created main method

---
+++ Unit4Lab2_CircleLab/Circle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit4Lab2_CircleLab
+{
+    public class Circle
+    {
+        public double Radius { get; private set; }
+        public double Circumference;
+        public double Area;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateCircumference()
+        {
+            return (2 * Math.PI * Radius);
+        }
+
+        public string CalculateFormattedCircumference(Circle thisCircle)
+        {
+            return $"The Circumference is {CalculateCircumference()}";
+        }
+
+        public double CalculateArea()
+        {
+            return Area;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            return "";
+        }
+
+        private string FormatNumber(double x)
+        {
+            return x.ToString();
+        }
+    }
+}
--- Unit4Lab2_CircleLab/Program.cs
+++ Unit4Lab2_CircleLab/Program.cs
@@ -1,36 +1,86 @@
-﻿public class Circle 
+﻿using System.Text.RegularExpressions;
+using Unit4Lab2_CircleLab;
+
+List<Circle> allCircles = new List<Circle>();
+
+Console.WriteLine("Welcome to the Circle Objects Program!");
+PauseAndClearScreen();
+
+bool runningProgram = true;
+while (runningProgram)
 {
-    public double Radius;
-    public double Circumference;
-    public double Area;
-
-    public Circle(double radius)
+    bool calculatingCircleStuff = true;
+    while (calculatingCircleStuff)
     {
-        Radius = radius;
+        Console.Write("Enter the radius of your circle: ");
+        string userRadius = Console.ReadLine();
+        bool isANumber = double.TryParse(userRadius, out double theRadius);
+        if (isANumber)
+        {
+            if (theRadius > 0)
+            {
+                Circle thisCircle = new Circle(theRadius);
+                thisCircle.CalculateCircumference();
+                Console.WriteLine();
+                Console.WriteLine(thisCircle.CalculateFormattedCircumference(thisCircle));
+                PauseAndClearScreen();
+                allCircles.Add(thisCircle);
+                calculatingCircleStuff = false;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, we need a number greater than zero to calculate a Circumference. Please try again.");
+                Console.WriteLine("");
+            }
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Sorry, that doesn't appear to be a number. Let's try again.");
+            PauseAndClearScreen();
+        }
     }
+    runningProgram = WannRestart();
+}
+Console.WriteLine("Thank you for using the Circle Object Program!");
+Console.WriteLine("Goodbye...");
 
-    public double CalculateCircumference()
+static void PauseAndClearScreen()
+{
+    Console.WriteLine();
+    Console.WriteLine("Press Enter to Continue.");
+    Console.ReadLine();
+    Console.Clear();
+}
+static bool WannRestart()
+{
+    bool askingUser = true;
+    string yesInput = "[yY]";
+    string noInput = "[nN]";
+    while (askingUser)
     {
-        return Circumference;
+        Console.WriteLine("Would you like to calculate the information for another circle?");
+        Console.WriteLine("Enter 'Yes' or 'No' below.");
+        Console.Write("Your Choice: ");
+        string userChoice = Console.ReadLine();
+        if (Regex.IsMatch(userChoice, yesInput))
+        {
+            PauseAndClearScreen();
+            return true;
+        }
+        else if (Regex.IsMatch(userChoice, noInput))
+        {
+            PauseAndClearScreen();
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Sorry, I didn't understand that choice. Please try again.");
+            Console.ResetColor();
+            Console.WriteLine("");
+        }
     }
-
-    public string CalculateFormattedCircumference()
-    {
-        return "";
-    }
-
-    public double CalculateArea()
-    {
-        return Area;
-    }
-
-    public string CalculateFormattedArea()
-    {
-        return "";
-    }
-
-    private string FormatNumber(double x)
-    {
-        return x.ToString();
-    }
+    return false;
 }
